f0c63a60bc46083ee2549ce2b62506151e229cda
Mike Williamson <mike@korora.ca>
2017-07-12 20:27:32 -0400

    Make test description nicer.

--- test/App.test.js
+++ test/App.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { mount, shallow } from 'enzyme'
 import App from '../src/App'
 
-describe('App component', () => {
+describe('<App />', () => {
   it('renders text', () => {
     const app = shallow(<App />)
     expect(app.text()).toContain('Spothire')
