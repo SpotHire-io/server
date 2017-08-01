c9ffc5634d74fb89832859c0f715dc585f84d553
Mike Williamson <mike@korora.ca>
2017-08-01 00:46:19 -0400

    Run api tests in their own database

--- test/api.test.js
+++ test/api.test.js
@@ -1,22 +1,65 @@
 import request from 'supertest'
 import graphqlHTTP from 'express-graphql'
-import Server from '../src/server'
 import jwt from 'jsonwebtoken'
+import { Database } from 'arangojs'
+import Connection from '../src/database'
+import Server from '../src/server'
 
-let userMock = jest
-  .fn()
-  .mockReturnValueOnce({ email: 'mike@korora.ca', name: 'mike' })
+// About the ugliness:
+// Since Jest runs tests in parallel, each file needs to create it's own db,
+// otherwise test data from one test clobbers test data in another. The result
+// nondeterministic tests and general unhappiness.
+// TODO: This will need to be cleaned up and most likely shared across tests.
+
+const username = process.env.ARANGODB_USERNAME
+const password = process.env.ARANGODB_PASSWORD
+
+function getFilenameFromPath(path) {
+  return path.split('/').slice(-1).join()
+}
+
+function dbNameFromFile(filename) {
+  return getFilenameFromPath(filename).replace(/\./g, '_') + '_' + Date.now()
+}
 
 const secret = 'secret'
+const url = `http://${username}:${password}@arangodb:8529`
+const conn = new Database({ url })
+const dbname = dbNameFromFile(__filename)
 
-const server = Server(
-  {
-    getUserByEmail: userMock,
-  },
-  secret
-)
+let db, server, testdb, usersCollection
+
+const user = { email: 'mike@korora.ca', name: 'mike' }
 
 describe('GraphQL API', () => {
+  beforeAll(async () => {
+    //create a generic connection to the db
+    await conn.createDatabase(dbname, [{ username, password }])
+    //create a database to run the tests in
+    testdb = new Database({ databaseName: dbname, url })
+    //create a users collection instance
+    usersCollection = testdb.collection('users')
+    //create the collection in the database
+    await usersCollection.create()
+
+    server = Server(
+      Connection(
+        new Database({
+          databaseName: dbname,
+          url,
+        })
+      ),
+      secret
+    )
+  })
+
+  afterAll(async () => {
+    await conn.dropDatabase(dbname)
+  })
+
+  afterEach(async () => {
+    await testdb.truncate()
+  })
   describe('without an auth token', () => {
     it('is properly mounted at /graphql', async () => {
       let response = await request(server)
@@ -64,6 +107,8 @@ describe('GraphQL API', () => {
 
     describe('user(email: "a@b.c")', () => {
       it('finds a user by email', async () => {
+        await usersCollection.save(user)
+
         let response = await request(server)
           .post('/graphql')
           .set('Authorization', 'Bearer ' + token)
