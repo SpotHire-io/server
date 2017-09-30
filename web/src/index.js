1b72f0f129b1645cd529a91e064981e119a6bfd7
Mike Williamson <mike@korora.ca>
2017-09-29 23:50:08 -0400

    Add coming soon page.

--- web/src/index.js
+++ web/src/index.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import Logo from './components/Logo'
 import { injectGlobal } from 'styled-components'
-import Title from './components/Title'
 
 injectGlobal`
-  body {
-    margin: 0;
-  }
+	body {
+		margin: 0;
+		background-color: #5ab6b2;
+    color: #fff;
+    font-family: sans-serif;
+	}
+  main {
+		padding: 4em;:w
+		text-align: center;
+	}
 `
 
 class App extends Component {
-
   render() {
     return (
-      <main>
-        <Title>Spothire</Title>
+      <main style={{}}>
+        <Logo width="40em" />
+        <h2>Coming soon.</h2>
       </main>
     )
   }
