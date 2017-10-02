7c222d8e95dd10d369019c6e42816538180ce653
Mike Williamson <mike@korora.ca>
2017-10-01 22:45:14 -0400

    Replace styled-components with Emotion
    This looks like a faster, lighter-weight library with a similar API.
    Let's give it a try!

--- web/src/components/Title.js
+++ web/src/components/Title.js
@@ -1,6 +1,6 @@
-import styled from 'styled-components'
+import styled from 'react-emotion'
 
-const Title = styled.h2`
+const Title = styled('h2')`
   margin-top: 0;
   margin-bottom: 0;
   font-size: 1.12em;
--- web/src/index.js
+++ web/src/index.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import Logo from './components/Logo'
-import { injectGlobal } from 'styled-components'
+import { css, injectGlobal } from 'emotion'
 
 injectGlobal`
 	body {
 		margin: 0;
-		background-color: #5ab6b2;
-    color: #fff;
-    font-family: sans-serif;
+    background-color: #5ab6b2;
+    height: 100%;
 	}
-  main {
-		padding: 4em;:w
-		text-align: center;
-	}
+`
+const white = css`color: #fff;`
+
+const mainstyle = css`
+  font-family: sans-serif;
+  padding: 4em;
+  text-align: center;
+  height: 100%;
 `
 
 class App extends Component {
   render() {
     return (
-      <main style={{}}>
+      <main className={mainstyle}>
         <Logo width="40em" />
-        <h2>Coming soon.</h2>
+        <h2 className={white}>Coming soon.</h2>
       </main>
     )
   }
