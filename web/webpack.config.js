ef455de62e9b1afa319c356bf4636a41edc32b37
Mike Williamson <mike.williamson@tbs-sct.gc.ca>
2017-10-14 15:19:50 -0400

    Create source-maps in development mode

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,22 +1,25 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: require('html-webpack-template'),
-      title: 'Spothire',
-      appMountId: 'app',
-    }),
-  ],
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+module.exports = env => {
+  return {
+    entry: './src/index.js',
+    devtool: env === 'development' ? 'source-map' : 'none',
+    output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: require('html-webpack-template'),
+        title: 'Spothire',
+        appMountId: 'app',
+      }),
     ],
-  },
+    module: {
+      loaders: [
+        { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      ],
+    },
+  }
 }
