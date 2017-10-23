981cdc45c7acf4d5fa3417e919674be14cc6656f
Mike Williamson <mike.williamson@tbs-sct.gc.ca>
2017-10-23 18:52:46 -0400

    Add SecondaryMenu component
    This component can be used like this:
    ```javascript
    <SecondaryMenu
      items={[
        {
          text: 'Item 1',
          href: '#item1',
        },
        {
          text: 'Item 2',
          href: '#item2',
        },
        {
          text: 'Item 3',
          href: '#item3',
        },
      ]}
    />
    ```

---
+++ web/src/components/SecondaryMenu.js
@@ -0,0 +1,133 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { css } from 'emotion'
+
+const bgWhite = css`
+  background-color: #fff;
+`
+const mb4 = css`
+  margin-bottom: 2rem;
+`
+const ph4 = css`
+  padding-left: 2rem;
+  padding-right: 2rem;
+`
+const secondaryMenuStyles = css`
+  ${bgWhite};
+  ${mb4};
+  ${ph4};
+`
+
+const b = css`
+  font-weight: bold;
+`
+const nearBlack = css`
+  color: #111;
+`
+const hoverGray = css`
+  &:hover {
+    color: #777;
+  }
+  &:focus {
+    color: #777;
+  }
+`
+const dib = css`
+  display: inline-block;
+`
+const pv3 = css`
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+`
+const mr4 = css`
+  margin-right: 2rem;
+`
+const noUnderline = css`
+  text-decoration: none;
+`
+const animateAll = css`
+  transition: all 0.15s ease-in;
+`
+const bb = css`
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+`
+const bw1 = css`
+  border-width: 0.125rem;
+`
+const hoverBWhite40 = css`
+  &:hover {
+    border-color: rgba(255, 255, 255, 0.4);
+  }
+`
+
+const bTeal = css`
+  border-color: #5ab6b2;
+`
+const bWhite40 = css`
+  border-color: rgba(255, 255, 255, 0.4);
+`
+
+const activeStyles = css`
+  ${bTeal};
+  ${bWhite40};
+`
+
+const styles = css`
+  ${b};
+  ${nearBlack};
+  ${hoverGray};
+  ${dib};
+  ${pv3};
+  ${mr4};
+  ${noUnderline};
+  ${animateAll};
+  ${bb};
+  ${bw1};
+  ${hoverBWhite40};
+  ${props => (props.active ? activeStyles : css``)};
+`
+
+/**
+ * Secondary section menu.
+ *
+ * Provides navigation for a given section.
+ */
+export default class SecondaryMenu extends React.Component {
+  renderNavItem = (item, index) => {
+    return (
+      <a
+        key={item + index}
+        onClick={() => this.props.onClick(item.key)}
+        href={item.href}
+        className={styles}
+        style={{ marginBottom: '-0.125rem' }}>
+        {item.text}
+      </a>
+    )
+  }
+
+  render() {
+    const wrapperClasses = css`
+      ${bb};
+      ${bgWhite};
+      ${bw1};
+      ${bTeal};
+      ${props => (props.className ? props.classNames : css``)};
+    `
+
+    return (
+      <nav className={wrapperClasses}>
+        {this.props.items.map((item, index) => this.renderNavItem(item, index))}
+      </nav>
+    )
+  }
+}
+SecondaryMenu.defaultProps = {
+  className: secondaryMenuStyles,
+}
+
+SecondaryMenu.propTypes = {
+  onClick: PropTypes.func,
+  items: PropTypes.array,
+}
